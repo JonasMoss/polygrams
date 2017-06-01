12d640a6869e4a89be9e1c0f2725cd1edf110a25
JonasMoss <jonas.gjertsen@gmail.com>
2017-06-01 09:07:47 +0200

    Added ArmadilloRcpp
    ArmadilloRcpp added to the project, used for calculating the objective
    vector.

---
+++ R/help.R
@@ -0,0 +1,9 @@
+#'polygrams
+#'
+#'@author Jonas Moss <jonas.moss@gmail.com>
+#'@docType package
+#'@name polygrams
+#'@importFrom Rcpp evalCpp
+#'@useDynLib polygrams
+#'@exportPattern "^[[:alpha:]]+"
+NULL
